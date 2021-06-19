c04b50678b62ef8cb224ee52d65d5022041035b8
ahmedatefmohamed <43454769+ahmedatefmohamed@users.noreply.github.com>
2021-06-19 02:07:20 +0200

    partner Comp. Form added

--- src/app/Components/Partener/partener/partener.component.ts
+++ src/app/Components/Partener/partener/partener.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { NavbarService } from 'src/app/Services/Home/navbar.service';
+
+declare var OnNextClick: any;
+// declare var Onload: any;
+declare var OnPrevious: any;
 
 @Component({
   selector: 'app-partener',
@@ -6,10 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partener.component.css']
 })
 export class PartenerComponent implements OnInit {
+  isLinear = true;
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder, public nav: NavbarService) { }
 
   ngOnInit(): void {
-  }
+    // new Onload();
+    // new OnNextClick();
+    this.nav.hide();
 
+    this.firstFormGroup = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      emailAddress: ['', Validators.required],
+
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      storeName: ['', Validators.required],
+      numBranchNo: ['', Validators.required],
+      contacts: ['', Validators.required],
+      storeAddress: ['', Validators.required],
+
+    });
+
+  }
+    submit(){
+      console.log(this.firstFormGroup.value);
+      console.log(this.secondFormGroup.value);
+  }
 }
