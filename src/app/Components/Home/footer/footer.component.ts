f5aa758f1229031692ad6c0369a3d81e38aa0d61
SondosAwad <77900759+SondosAwad@users.noreply.github.com>
2021-06-30 23:33:41 +0200

    nfsy nkhlas

--- src/app/Components/Home/footer/footer.component.ts
+++ src/app/Components/Home/footer/footer.component.ts
@@ -2,22 +2,23 @@ import { FooterService } from './../../../Services/Home/footer.service';
 import { Component, OnInit } from '@angular/core';
 import { Store } from 'src/app/Models/Store';
 
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  Cuisines: any;
+  Stores: any;
 
-  constructor( private _footerservice:FooterService) { 
-  }
+  constructor(private _footerservice: FooterService) {}
 
   ngOnInit(): void {
-    this.MostCommonStores()
-  }
-  stores! : Store [];
-  MostCommonStores(){
-      console.log (this._footerservice.getstores());
+    this._footerservice.getCuisines().subscribe((Cuisines) => {
+      this.Cuisines = Cuisines;
+    });
+    this._footerservice.getStores().subscribe((Stores) => {
+      this.Stores = Stores;
+    });
   }
 }
--- src/app/Services/Home/footer.service.ts
+++ src/app/Services/Home/footer.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Store} from '../../Models/Store';
+import { Store } from '../../Models/Store';
 import { Observable } from 'rxjs';
-const API ="https://localhost:44311/api/Stores";
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FooterService {
-
-  _Stores : Store | undefined;
   constructor(private http: HttpClient) {}
-  
-  getstores(){
-    console.log(API)
-    return this.http.get(API);
-   }
 
-      
+  getStores() {
+    return this.http.get('https://localhost:44311/api/Stores/MostCommonStores');
+  }
+  getCuisines() {
+    return this.http.get(
+      'https://localhost:44311/api/Cuisines/MostCommonCuisine'
+    );
+  }
 }
