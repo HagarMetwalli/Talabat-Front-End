ba3d6c9de9cb7bea68e6d11286559354f39862c9
ReemFahmi <56454294+ReemFahmi@users.noreply.github.com>
2021-07-16 19:37:43 +0200

    Revert "Revert "Update app.module.ts""
    This reverts commit 779dcb1d5f7ab39169baeefe1562720025fcd0a1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -151,7 +151,7 @@ import { TopPageScrollComponent } from './Components/top-page-scroll/top-page-sc
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCUzKQYbhsvR6K__DdrqYTJiSnuXpDNsWE',
+      apiKey: 'AIzaSyCUzKQYbhsvR6K__xxxxxxxxxxxxxxxxxx',
       libraries: ['places'],
     }),
     MatExpansionModule,
